b19077e158d91df69df52dbc55560cd5c0f49cc3
zarc1411 <zarc1483@gmail.com>
2021-03-21 09:00:13 +0530

    Added dotfiles

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "Iosevka:style=Medium:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = { "Iosevka:style=Medium:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
